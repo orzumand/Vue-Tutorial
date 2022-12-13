09667365e735dbaa232175d5204e30d1b4d95040
orzumand <hayotbekyoldashev0@gmail.com>
2022-12-13 18:09:59 +0500

    App file structure

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!-- vue file 3 qisimdan iborat boladi  -->
+
+<!-- 1 template html code yozsh uchun  -->
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+<!-- 2 script javascript code yozish uchun -->
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello Vue",
+    };
+  },
+};
+</script>
+<!-- 3 style css yozish uchun  -->
+<style></style>
